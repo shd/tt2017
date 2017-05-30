237a7bef5958432688eea5eb626a37c52b384d76
Titova Sophia <sof_titova@rambler.ru>
2017-05-30 18:42:01 +0300

    small changes
    Return type of algorithm W has been changed.

--- hw2_inference.mli
+++ hw2_inference.mli
@@ -5,4 +5,4 @@ val infer_simp_type : lambda -> ((string * simp_type list) * simp_type) option
 
 type hm_lambda = HM_Var of string | HM_Abs of string * lambda | HM_App of lambda * lambda | HM_Let of string * lambda * lambda
 type hm_type = HM_Elem of string | HM_Arrow of hm_type * hm_type | HM_ForAll of string * hm_type
-val algorithm_w : hm_lambda -> ((string * hm_type list) * hm_type) option
+val algorithm_w : hm_lambda -> ((string * hm_type) list * hm_type) option
